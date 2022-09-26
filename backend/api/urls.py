494e58bede3b395b3d09df86f98d595535b9a121
nishi7409 <nishi7409@gmail.com>
2022-09-26 18:19:22 -0400

    update

---
+++ backend/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from .views import *
+from rest_framework import permissions
+
+# all api endpoints
+urlpatterns = [
+    path('test', Test.as_view()),
+]
